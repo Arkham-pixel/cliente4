d5de4336846e6b69b0ed823980c8bfad581be2e5
oscar <oscaraox4512@gmail.com>
2025-07-17 09:15:18 -0500

    oyt

--- backend/backend/backend/controllers/estadoController.js
+++ backend/backend/backend/controllers/estadoController.js
@@ -1,12 +1,4 @@
-import mongoose from 'mongoose';
-import secondaryConnection from '../db/secondaryConnection.js';
-
-const EstadoSchema = new mongoose.Schema({
-  codiEstado: Number,
-  descEstado: String
-}, { collection: 'gsk3cAppestados' });
-
-const Estado = secondaryConnection.model('Estado', EstadoSchema);
+import Estado from '../models/Estado.js';
 
 export const obtenerEstados = async (req, res) => {
   try {
